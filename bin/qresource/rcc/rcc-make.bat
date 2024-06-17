c5fa2ce5238ae33386bf05bf401a069e272074b0
Jimo <edgar.corbello.pereira@gmail.com>
2024-06-17 14:32:28 -0300

    remove unwanted files

--- bin/qresource/rcc/rcc-make.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-set rcc=rcc.exe
-set opt=--verbose --compress-algo zlib --format-version 1 --binary
-
-%rcc% %opt% ./../qrc/graphics_resources.qrc -o ./../rcc/graphics_resources.rcc
-
-pause
